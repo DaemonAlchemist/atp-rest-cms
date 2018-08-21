18bc01a8bb5c9864d006ae6666b8af734e003bfb
Andrew Wittrock <andy@wittrock.us>
2018-08-21 17:02:23 -0500

    Add tag filtering

--- src/model/content.js
+++ src/model/content.js
@@ -1,9 +1,14 @@
 
 import {Entity} from 'atp-active-record';
+import {filterOnTags} from "atp-rest-tag";
 
 export default class Content extends Entity
 {
     constructor() {
         super('cms', 'atpcms_content');
+    }
+
+    filter(filters) {
+        return super.filter(filterOnTags(this, filters, 'cmsContent'));
     }
 }
